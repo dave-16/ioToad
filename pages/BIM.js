184a47ae823402dafd26eb4fb68b243b9f9b3400
Tsega Debebe <tsegadebebe1@gmail.com>
2022-08-17 15:20:56 +0000

    Update BIM.js

--- pages/BIM.js
+++ pages/BIM.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import {PageBackground} from '../components/background.js';
 import {Header} from '../components/header';
 import {Footer} from '../components/footer';
